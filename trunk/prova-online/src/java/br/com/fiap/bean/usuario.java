97e17bc7550ce61e4b9a7f7ec3306e2e7704fe7b
pasqualini9@gmail.com <pasqualini9@gmail.com@d68f7a85-5fa0-e8d2-d34d-ef6495331211>
2011-09-17 14:29:38 +0000

    Adicionado as classe de beans (é bem provavel que serão modificadas posteriormente)

---
+++ trunk/prova-online/src/java/br/com/fiap/bean/usuario.java
@@ -0,0 +1,58 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.fiap.bean;
+
+/**
+ *
+ * @author Vinicius
+ */
+public class usuario {
+    private String login;
+    private String senha;
+    private String nome;
+    
+    /**
+     * O id refere-se ao tipo de usuário
+     * 1-Aluno
+     * 2-Professor
+     * 3-Administrador
+     */
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    
+    
+}
