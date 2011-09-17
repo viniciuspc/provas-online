081a4ea5e6cea816b11f9db726954492aaf986b2
pasqualini9@gmail.com <pasqualini9@gmail.com@d68f7a85-5fa0-e8d2-d34d-ef6495331211>
2011-09-17 14:29:38 +0000

    Adicionado as classe de beans (é bem provavel que serão modificadas posteriormente)

---
+++ prova-online/src/java/br/com/fiap/bean/aluno.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.fiap.bean;
+
+/**
+ *
+ * @author Vinicius
+ */
+public class aluno extends usuario {
+    private double nota;
+    
+    
+
+   
+    
+    
+}
---
+++ prova-online/src/java/br/com/fiap/bean/professor.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.fiap.bean;
+
+/**
+ *
+ * @author Vinicius
+ */
+public class professor extends usuario {
+    
+}
