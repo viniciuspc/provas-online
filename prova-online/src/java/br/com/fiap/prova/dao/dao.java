2ace4d0e6189b5a25bcc276bc28ce68ebe31f81d
lsromeiro@gmail.com <lsromeiro@gmail.com@d68f7a85-5fa0-e8d2-d34d-ef6495331211>
2011-09-25 18:16:25 +0000

    1- alteracao de nome de todas as classes (primeira letra minuscula para maiuscula) 2- criado interface de comunicacao com o banco 3- modelagem do banco

---
+++ prova-online/src/java/br/com/fiap/prova/dao/dao.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.fiap.prova.dao;
+import java.sql.*;
+
+/**
+ * Classe responsavel pela execução de querys no banco de daodos
+ *
+ * @author Leonardo
+ */
+
+public class Dao {
+    protected Connection cn;
+    protected PreparedStatement st;
+    protected ResultSet rs;
+
+    protected boolean abrir() throws Exception{
+        try{
+            Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+            cn = DriverManager.getConnection("jdbc:derby://localhost:1527/provaonline?user=dbuser&password=DBUser4me...");
+            return true;
+        }
+        catch(Exception ex){
+            //return false;
+            throw ex;
+        }
+    }
+ 
+    protected void fechar(){
+        try{
+            cn.close();
+        }
+        catch(Exception ex){}
+    }
+}
