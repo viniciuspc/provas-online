67d444f72c471e825f8f506d1f3ab8d68e547a5e
lsromeiro@gmail.com <lsromeiro@gmail.com@d68f7a85-5fa0-e8d2-d34d-ef6495331211>
2011-09-25 18:08:41 +0000

    1- alteracao de nome de todas as classes (primeira letra minuscula para maiuscula) 2- criado interface de comunicacao com o banco 3- modelagem do banco

---
+++ prova-online/src/java/br/com/fiap/prova/dao/DaoUsuario.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.fiap.prova.dao;
+import br.com.fiap.prova.bean.usuarios.Usuario;
+/**
+ *
+ * @author Leonardo
+ */
+public class DaoUsuario extends Dao {
+    private Usuario usuario;
+     
+    /*
+     * cadatraUsuario
+     * procuraUsuario
+     * autenticaUsuario
+     * 
+     */
+    
+}
