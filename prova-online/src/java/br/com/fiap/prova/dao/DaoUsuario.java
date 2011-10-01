452921b275656fa47281d85d97d1fb3734fff81e
renanbenedicto@gmail.com <renanbenedicto@gmail.com@d68f7a85-5fa0-e8d2-d34d-ef6495331211>
2011-10-01 20:01:05 +0000

    Criação do método listarUsuarios na classe DaoUsuario, e no bean Usuario. Datagrid inserido na página do Administrador para mostrar usuarios. (Ainda não funcionando)

--- prova-online/src/java/br/com/fiap/prova/dao/DaoUsuario.java
+++ prova-online/src/java/br/com/fiap/prova/dao/DaoUsuario.java
@@ -4,6 +4,8 @@
  */
 package br.com.fiap.prova.dao;
 import br.com.fiap.prova.bean.usuarios.Usuario;
+import java.sql.SQLException;
+import java.util.*;
 /**
  *
  * @author Leonardo
@@ -17,5 +19,37 @@ public class DaoUsuario extends SqlCore {
      * autenticaUsuario
      * 
      */
+    
+    public ArrayList<Usuario> listarUsuarios() throws SQLException{
+        
+        ArrayList<Usuario> listaUsuarios = new ArrayList<Usuario>();
+        
+        try {
+            st = cn.prepareStatement("SELECT * FROM aluno")  ;   
+            rs = st.executeQuery();
+            int i = 0;
+            while (rs.next()) {
+
+                usuario = new Usuario();
+
+                usuario.setLogin( rs.getString(1) );
+                usuario.setNome( rs.getString(2) );
+                usuario.setSenha( rs.getString(3) );
+                usuario.setTipo( rs.getInt(4) );
+                listaUsuarios.add(usuario);
+
+                i++;
+            }
+        }
+        catch(SQLException ex) {
+            System.out.println("Erro ao listar usuario do BD");
+            ex.printStackTrace();
+        }
+        /*
+         * Trazer do banco de dados.
+        */
+        
+        return listaUsuarios;
+    }
     
 }
